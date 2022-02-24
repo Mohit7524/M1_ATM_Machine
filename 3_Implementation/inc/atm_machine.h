6b257539e2abd29eaace5d79e4a58edddfcb3bca
Mohit7524 <mohit7524938692@gmail.com>
2022-02-24 08:05:29 +0530

    updated

--- 3_Implementation/inc/atm_machine.h
+++ 3_Implementation/inc/atm_machine.h
@@ -1,1 +1,26 @@
+/**
+ * @file atm_machine.h
+ * @author Mohit Kumar Yadav (mohit7524938692@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<stdbool.h>
+
+int main()
+{
+   int opt;
+   int choice;
+};
+
+  //#endif 
